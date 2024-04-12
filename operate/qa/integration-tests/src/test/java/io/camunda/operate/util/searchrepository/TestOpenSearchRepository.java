a91faa04bbdd1a49fa824ff12a78759736a41dd7
SvetlanaDorokhova <svetlana.dorokhova@camunda.com>
2024-04-12 12:27:35 +0200

    feat: position based update scripts for importer
    * store/read `concurrencyMode` in import-position index
    * use upsert with script in cse of concurrency mode on for `list-view` index
    
    Related with #6477
    
    Related with #6477

--- operate/qa/integration-tests/src/test/java/io/camunda/operate/util/searchrepository/TestOpenSearchRepository.java
+++ operate/qa/integration-tests/src/test/java/io/camunda/operate/util/searchrepository/TestOpenSearchRepository.java
@@ -59,6 +59,7 @@ import java.util.stream.Collectors;
 import org.opensearch.client.opensearch._types.OpenSearchException;
 import org.opensearch.client.opensearch._types.mapping.DynamicMapping;
 import org.opensearch.client.opensearch._types.query_dsl.Query;
+import org.opensearch.client.opensearch.core.IndexRequest.Builder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Component;
@@ -96,6 +97,14 @@ public class TestOpenSearchRepository implements TestSearchRepository {
   }
 
   @Override
+  public boolean createOrUpdateDocumentFromObject(
+      final String indexName, final String docId, final Object data, final String routing)
+      throws IOException {
+    final Map<String, Object> entityMap = objectMapper.convertValue(data, new TypeReference<>() {});
+    return createOrUpdateDocument(indexName, docId, entityMap, routing);
+  }
+
+  @Override
   public String createOrUpdateDocumentFromObject(final String indexName, final Object data)
       throws IOException {
     final Map<String, Object> entityMap = objectMapper.convertValue(data, new TypeReference<>() {});
@@ -104,10 +113,18 @@ public class TestOpenSearchRepository implements TestSearchRepository {
 
   @Override
   public boolean createOrUpdateDocument(
-      final String indexName, final String id, final Map<String, ?> doc) throws IOException {
-    return richOpenSearchClient
-        .doc()
-        .indexWithRetries(indexRequestBuilder(indexName).id(id).document(doc));
+      final String indexName, final String id, final Map<String, ?> doc) {
+    return createOrUpdateDocument(indexName, id, doc, null);
+  }
+
+  @Override
+  public boolean createOrUpdateDocument(
+      final String indexName, final String id, final Map<String, ?> doc, final String routing) {
+    final Builder<Object> document = indexRequestBuilder(indexName).id(id).document(doc);
+    if (routing != null) {
+      document.routing(routing);
+    }
+    return richOpenSearchClient.doc().indexWithRetries(document);
   }
 
   @Override
